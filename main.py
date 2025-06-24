da44c6a0cb8842b9bf13d6cb96e8777973fa05a1
Googluu <sneyder.h.rodriguez@gmail.com>
2025-06-24 16:08:49 -0500

    body - modelos anidados

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from enum import Enum
 from typing import Union, Literal
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, HttpUrl
 
 from fastapi import FastAPI, Query, Path, Body
 
@@ -365,16 +365,64 @@ class User(BaseModel):
 #     results = {"item_id": item_id, "item": item}
 #     return results
 
-class Item(BaseModel):
-    name: str
-    description: str | None = Field(
-        default=None, title="The description of the item", max_length=300
-    )
-    price: float = Field(gt=0, description="The price must be greater than zero")
-    tax: float | None = None
+# class Item(BaseModel):
+#     name: str
+#     description: str | None = Field(
+#         default=None, title="The description of the item", max_length=300
+#     )
+#     price: float = Field(gt=0, description="The price must be greater than zero")
+#     tax: float | None = None
 
 
-@app.put("/items/{item_id}")
-async def update_item(item_id: int, item: Annotated[Item, Body(embed=True)]):
-    results = {"item_id": item_id, "item": item}
-    return results
+# @app.put("/items/{item_id}")
+# async def update_item(item_id: int, item: Annotated[Item, Body(embed=True)]):
+#     results = {"item_id": item_id, "item": item}
+#     return results
+
+# class Image(BaseModel):
+#     url: HttpUrl
+#     name: str
+
+
+# class Item(BaseModel):
+#     name: str
+#     description: str | None = None
+#     price: float
+#     tax: float | None = None
+#     tags: set[str] = set()
+#     images: list[Image] | None = None
+
+
+# @app.put("/items/{item_id}")
+# async def update_item(item_id: int, item: Item):
+#     results = {"item_id": item_id, "item": item}
+#     return results
+
+# class Image(BaseModel):
+#     url: HttpUrl
+#     name: str
+
+
+# class Item(BaseModel):
+#     name: str
+#     description: str | None = None
+#     price: float
+#     tax: float | None = None
+#     tags: set[str] = set()
+#     images: list[Image] | None = None
+
+
+# class Offer(BaseModel):
+#     name: str
+#     description: str | None = None
+#     price: float
+#     items: list[Item]
+
+
+# @app.post("/offers/")
+# async def create_offer(offer: Offer):
+#     return offer
+
+@app.post("/index-weights/")
+async def create_index_weights(weights: dict[int, float]):
+    return weights
